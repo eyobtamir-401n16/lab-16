508a73716eb39d0dc73ce064820c2f554584c27b
Eyob1984 <tamireyob@gmail.com>
2020-05-11 15:51:38 -0700

    final work

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 'use strict';
 
 const emitter = require('./lib/events.js');
+const vendorHandler1 = require('./lib/handlers.js').vendorHandler;
+const inTransitHandler1 = require('./lib/handlers.js').inTransitHandler; 
+const delveredHandler1 = require('./lib/handlers.js').deliveredHandler;
+
+
+
+
+emitter.on('new-order', vendorHandler1);
+emitter.on('in-transit', inTransitHandler1);
+emitter.on('delivered', delveredHandler1);
 
 
 
